2b21dc5ba39d2a588ac80cb0251edc25430a5c43
Lukas Stracke <l.stracke@student.tugraz.at>
2020-03-18 10:39:09 +0100

    COOK-12 Add basic Android project with TabView Activity [SL, LH]

---
+++ app/app/src/main/java/at/tugraz/ist/sw20/mam3/cook/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package at.tugraz.ist.sw20.mam3.cook.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
