ce3fe264395c9ffb794cd15afd5f9e0df70780c9
Hannah Lederer <hannah.lederer@student.tugraz.at>
2020-04-01 11:07:10 +0200

    COOK-28 display favorite recipes [LH, WP]

--- app/app/src/main/java/at/tugraz/ist/sw20/mam3/cook/ui/favourites/FavouritesFragment.kt
+++ app/app/src/main/java/at/tugraz/ist/sw20/mam3/cook/ui/favourites/FavouritesFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import at.tugraz.ist.sw20.mam3.cook.R
+import at.tugraz.ist.sw20.mam3.cook.model.entities.Recipe
+import at.tugraz.ist.sw20.mam3.cook.model.service.DataReadyListener
+import at.tugraz.ist.sw20.mam3.cook.model.service.RecipeService
+import at.tugraz.ist.sw20.mam3.cook.ui.recipes.adapters.RecipeAdapter
 
 class FavouritesFragment : Fragment() {
 
     private lateinit var favouritesViewModel: FavouritesViewModel
+    private lateinit var lvFavorites: ListView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,10 +28,21 @@ class FavouritesFragment : Fragment() {
         favouritesViewModel =
                 ViewModelProvider(this).get(FavouritesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_favourites, container, false)
-        val textView: TextView = root.findViewById(R.id.text_favourites)
-        favouritesViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        lvFavorites = root.findViewById(R.id.list_favorites)
+
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        favouritesViewModel = ViewModelProvider(this).get(FavouritesViewModel::class.java)
+
+        val readyListener = object : DataReadyListener<List<Recipe>> {
+            override fun onDataReady(data: List<Recipe>?) {
+                lvFavorites.adapter = RecipeAdapter(context!!, data ?: listOf())
+            }
+        }
+
+        RecipeService(context!!).getFavoriteRecipes(readyListener)
     }
 }
