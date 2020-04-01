a34e11fd2ff5404844e317d9e4786a9a7553a6d5
Paul Weissensteiner <paul.weissensteiner@student.tugraz.at>
2020-04-01 08:24:28 +0200

    COOK-19 display recipe elements in a list on the main screen [WP, LH]

--- app/app/src/main/java/at/tugraz/ist/sw20/mam3/cook/ui/recipes/RecipesFragment.kt
+++ app/app/src/main/java/at/tugraz/ist/sw20/mam3/cook/ui/recipes/RecipesFragment.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import at.tugraz.ist.sw20.mam3.cook.R
+import at.tugraz.ist.sw20.mam3.cook.model.entities.Recipe
 import at.tugraz.ist.sw20.mam3.cook.ui.add_recipes.AddRecipesFragment
+import at.tugraz.ist.sw20.mam3.cook.ui.recipes.adapters.RecipeAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class RecipesFragment : Fragment() {
 
     private lateinit var recipesViewModel: RecipesViewModel
+
+    private lateinit var lvRecipes : ListView
+    private lateinit var recipes : List<Recipe>
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -25,10 +31,7 @@ class RecipesFragment : Fragment() {
         recipesViewModel =
                 ViewModelProvider(this).get(RecipesViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_recipes, container, false)
-        val textView: TextView = root.findViewById(R.id.text_recipes)
-        recipesViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        lvRecipes = root.findViewById(R.id.list_recipes)
 
         val floatingButton: FloatingActionButton = root.findViewById(R.id.item_add_button)
         floatingButton.setOnClickListener {
@@ -40,5 +43,16 @@ class RecipesFragment : Fragment() {
             fragmentTransaction.commit()
         }
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        recipesViewModel = ViewModelProvider(this).get(RecipesViewModel::class.java)
+
+        var recipe : Recipe = Recipe(-1, "Pizza","#Meat", 20, 30, false)
+        recipes = listOf(recipe, recipe, recipe, recipe, recipe, recipe, recipe, recipe)
+
+
+        lvRecipes.adapter = RecipeAdapter(this.context!!, recipes)
     }
 }
