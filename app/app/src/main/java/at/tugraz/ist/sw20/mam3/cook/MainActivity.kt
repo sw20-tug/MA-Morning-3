b19f018e43d8fd43677d31b9f765e55b099a09ff
amelak9 <amela.kurtic@student.tugraz.at>
2020-05-20 11:46:17 +0200

    COOK-39 Added about screen [KA, WP]

--- app/app/src/main/java/at/tugraz/ist/sw20/mam3/cook/MainActivity.kt
+++ app/app/src/main/java/at/tugraz/ist/sw20/mam3/cook/MainActivity.kt
@@ -2,20 +2,20 @@ package at.tugraz.ist.sw20.mam3.cook
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import at.tugraz.ist.sw20.mam3.cook.model.entities.Ingredient
 import at.tugraz.ist.sw20.mam3.cook.model.entities.Recipe
-import at.tugraz.ist.sw20.mam3.cook.model.entities.Step
 import at.tugraz.ist.sw20.mam3.cook.model.service.DataReadyListener
 import at.tugraz.ist.sw20.mam3.cook.model.service.RecipeService
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : AppCompatActivity() {
@@ -49,7 +49,7 @@ class MainActivity : AppCompatActivity() {
                 listv = data as MutableList<Recipe>
             }
         })
-        recipeService.getFavoriteRecipes(object : DataReadyListener<List<Recipe>> {
+        recipeService.getFavouriteRecipes(object : DataReadyListener<List<Recipe>> {
             override fun onDataReady(data: List<Recipe>?) {
                 Log.println(
                     Log.INFO, "Hi this is the special output asdfjölasdflak",
@@ -62,7 +62,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_search_filter, menu)
+        val si = menu?.findItem(R.id.about) as MenuItem
+       // val sv = si.getActionView() as TextView
 
+        si.setOnMenuItemClickListener() {
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+            true
+        }
         return true
     }
 }
