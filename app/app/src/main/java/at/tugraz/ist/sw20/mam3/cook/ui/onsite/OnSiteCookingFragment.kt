ef97da706aa01ff64d958b9a66a9816ff3e85f12
Lukas Stracke <l.stracke@student.tugraz.at>
2020-06-03 09:53:29 +0200

    COOK-11 populated photo recyclerView in Onsite Cooking Fragment [KA, SL]

--- app/app/src/main/java/at/tugraz/ist/sw20/mam3/cook/ui/onsite/OnSiteCookingFragment.kt
+++ app/app/src/main/java/at/tugraz/ist/sw20/mam3/cook/ui/onsite/OnSiteCookingFragment.kt
@@ -9,16 +9,21 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import at.tugraz.ist.sw20.mam3.cook.R
 import at.tugraz.ist.sw20.mam3.cook.model.entities.Recipe
 import at.tugraz.ist.sw20.mam3.cook.model.entities.Step
 import at.tugraz.ist.sw20.mam3.cook.model.service.DataReadyListener
 import at.tugraz.ist.sw20.mam3.cook.model.service.RecipeService
+import at.tugraz.ist.sw20.mam3.cook.ui.recipe_detail.adapters.ImageAdapter
 
 class OnSiteCookingFragment: Fragment() {
     private var recipeID: Long? = -1L
     private lateinit var recipeService: RecipeService
     private lateinit var root: View
+
+    private lateinit var recipe: Recipe
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,8 +44,10 @@ class OnSiteCookingFragment: Fragment() {
         if (recipeID != null && recipeID!! > -1L) {
             recipeService.getRecipeById(recipeID!!, object : DataReadyListener<Recipe> {
                 override fun onDataReady(data: Recipe?) {
-                    initElement(data!!.steps!!)
-
+                    recipe = data!!
+                    activity!!.runOnUiThread {
+                        initElement(data.steps!!)
+                    }
                 }
             })
         }
@@ -51,6 +58,19 @@ class OnSiteCookingFragment: Fragment() {
         val tvStepContent = root.findViewById<TextView>(R.id.tv_onsite_step_content)
         val btnNext = root.findViewById<Button>(R.id.btn_onsite_next)
         val btnBack = root.findViewById<Button>(R.id.btn_onsite_back)
+        val lvImages = root.findViewById<RecyclerView>(R.id.recipe_images)
+
+        lvImages.layoutManager = LinearLayoutManager(context,
+            LinearLayoutManager.HORIZONTAL, false)
+        lvImages.isNestedScrollingEnabled = true
+        lvImages.setHasFixedSize(false)
+
+        lvImages.adapter = ImageAdapter(context!!, recipe.photos ?: listOf())
+
+
+        if (steps.size == 1) {
+            btnNext.text = resources.getString(R.string.button_text_done)
+        }
 
         btnBack.visibility = View.GONE
         val step0 = steps[0]
